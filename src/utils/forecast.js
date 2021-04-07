47eb28ccd7579df210eb38cc000c808a8f1aca1f
Niall Mahford <niall.mahford@gmail.com>
2021-04-06 17:13:35 -0700

    JSON HTTP endpoint, fetch api, search form

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=fa80263e745153d2dea11dd463da79c3&query=${latitude},${longitude}&units=f`;
+
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service.')
+    } else if (body.error) {
+      callback('Unable to find location.')
+    } else {
+      callback(undefined, `It is currently ${body.current.temperature} degrees out. ${body.current.weather_descriptions[0]}. It feels like ${body.current.feelslike} degrees out.`)
+    }
+  })
+}
+
+module.exports = forecast
